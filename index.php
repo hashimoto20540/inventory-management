da65283461c98348fdac6514c30419dbf0d732d6
hashimoto20540 <hashikazu20540@gmail.com>
2024-05-06 23:52:50 +0900

    勉強のため不要な機能を追加（削除予定）

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-session_start();
+// session_start();
+$cookie_lifetime = 300;
+//セッションの有効期限を指定する
+session_start([
+    'cookie_lifetime' => $cookie_lifetime,
+]);
+
 $username = $_SESSION['name'];
 if (isset($_SESSION['id'])) {//ログインしているとき
     $msg = 'こんにちは' . htmlspecialchars($username, \ENT_QUOTES, 'UTF-8') . 'さん';
@@ -9,7 +15,19 @@ if (isset($_SESSION['id'])) {//ログインしているとき
     $msg = 'ログインしていません';
     $link = '<a href="login.php">ログイン</a>';
 }
+
+//isset()は関数がセットされているか返す関数
+if (!isset($_SESSION['count'])) {
+    // キー'count'が登録されていなければ、1を設定
+    $_SESSION['count'] = 1;
+} else {
+    //  キー'count'が登録されていれば、その値をインクリメント
+    $_SESSION['count']++;
+}
+
 ?>
 <h1><?php echo $msg; ?></h1>
+<?php echo $_SESSION['count']."回目の訪問です。"; ?><br>
 <?php echo $link_productList; ?><br>
 <?php echo $link_logout; ?>
+
